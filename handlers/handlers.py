d94c3de783701a74cf26585d3a3375fd26c0432b
FoxBigotry <152131156+FoxBigotry@users.noreply.github.com>
2024-05-07 15:23:03 +0700

    v5
    +WebApp

--- handlers/handlers.py
+++ handlers/handlers.py
@@ -1,31 +1,31 @@
 from aiogram import Router
 from aiogram.filters import CommandStart
-from aiogram.types import Message
-from aiogram.fsm.state import StatesGroup, State
-from aiogram.fsm.context import FSMContext
+from aiogram.types import Message, ContentType
 from database.db_connection import connect_insert
+
+from aiogram.types import KeyboardButton, WebAppInfo, ReplyKeyboardMarkup
 
 router = Router()
 
-class user_data(StatesGroup):
-    height = State()
-    weight = State()
 
 @router.message(CommandStart())
-async def step_one_height(message: Message, state: FSMContext):
-    await state.set_state(user_data.height)
-    await message.answer('Привет!\nМне нужно записать Ваши данные.\nВведите Ваш рост, в см')
+async def step_one_height(message: Message):
+    item1 = KeyboardButton(text='Анкета',web_app=WebAppInfo(url='https://7c1c-95-172-139-97.ngrok-free.app/webapp'))
+    keyboard = ReplyKeyboardMarkup(keyboard=[[item1]], resize_keyboard=True)
+    await message.answer('Привет!\nМне нужно записать Ваши данные.\nВоспользуйтесь кнопкой "Анкета"', reply_markup=keyboard)
 
-@router.message(user_data.height)
-async def step_two_weight(message: Message, state: FSMContext):
-    await state.update_data(height=message.text)
-    await state.set_state(user_data.weight)
-    await message.answer('Введите Ваш вес, в кг')
 
-@router.message(user_data.weight)
-async def step_three_save(message: Message, state: FSMContext):
-    await state.update_data(weight=message.text)
-    data = await state.get_data()
-    await connect_insert(message.from_user.id,data["height"],data["weight"])
-    await message.answer(f'Спасибо за Ваши данные.\nВаш id: {message.from_user.id}\nВаш рост: {data["height"]} см\nВаш вес: {data["weight"]} кг')
-    await state.clear()
+@router.message()
+async def handle_web_app_data(message: ContentType.WEB_APP_DATA):
+    web_app_data = message.web_app_data.data
+    data = web_app_data.split('&')
+    height_value = None
+    weight_value = None
+    for item in data:
+        key, value = item.split('=')
+        if key == 'height':
+            height_value = value
+        elif key == 'weight':
+            weight_value = value
+    await connect_insert(message.from_user.id, height_value, weight_value)
+    await message.answer(f'Спасибо за Ваши данные.\nВаш id: {message.from_user.id}\nВаш рост: {height_value} см\nВаш вес: {weight_value} кг')
